2d5125b8b8261fae1b2595c29e6cb81b4d3cdc53
Jaydeep Solanki <jaydp17@gmail.com>
2013-01-20 00:12:12 +0530

    First commit

---
+++ TheReader/LocalizedStrings.cs
@@ -0,0 +1,14 @@
+﻿using TheReader.Resources;
+
+namespace TheReader
+{
+    /// <summary>
+    /// Provides access to string resources.
+    /// </summary>
+    public class LocalizedStrings
+    {
+        private static AppResources _localizedResources = new AppResources();
+
+        public AppResources LocalizedResources { get { return _localizedResources; } }
+    }
+}
